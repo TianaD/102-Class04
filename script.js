dbb9344dbd39bb24ef2d36048b0345f4844b5e8a
TianaD <89160445+TianaD@users.noreply.github.com>
2023-02-14 20:35:33 -0600

    something

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let a = 1;
 
 let btnShowForm = document.getElementById("btn-show-form");
-function showForm () {
+    function showForm () {
 
     if (btnShowForm.style.display="hidden") {
 
@@ -25,53 +25,53 @@ let btnCloseForm = document.getElementById("btn-close-form");
 
 
 
-//without a funciton, i can still access variables outside of it
-//for this function, we get the type of alfredo we get. there are less options
-function test(){
-    let b = 5;
-    console.log (a);
-    console.log (b);
-}
+// //without a funciton, i can still access variables outside of it
+// //for this function, we get the type of alfredo we get. there are less options
+// function test(){
+//     let b = 5;
+//     console.log (a);
+//     console.log (b);
+// }
 
-//perameter will always be located inside the paraenthesis
-//these function are like alfredo whatever we pass into x, that is the meat for the alfredo. we are waiting to be told what kind of alfredo is being made
-    let lesson = function (x) {}
-        let b = 2;
+// //perameter will always be located inside the paraenthesis
+// //these function are like alfredo whatever we pass into x, that is the meat for the alfredo. we are waiting to be told what kind of alfredo is being made
+//     let lesson = function (x) {}
+//         let b = 2;
 
-        console.log(x + b);
-}
+//         console.log(x + b);
+// }
 
-lesson(a);
-l2();
+// lesson(a);
+// l2();
 
-function whodat() {
-    let name = prompt("What is your name?");
-    if (name != 'Tiana') {
-        alert("ERROR");
-    } else {
-        alert("Hello, " + name);
-    }
-
-    confirm()
-    
-}
-//console.log(b);
-
-//if i add numbers and nothing else, I will get an NAN (not a number) error. i.e. running lesson = undefined + 2 vs running lesson(3) = 5
-
-
-//email validation
-// function emailValidation() {
-//     let email = document.getElementById("eField").value;
-
-//     if (email.includes('@') && email=().includes('.')) {
-//             //true
-    
+// function whodat() {
+//     let name = prompt("What is your name?");
+//     if (name != 'Tiana') {
+//         alert("ERROR");
 //     } else {
-//         //false
+//         alert("Hello, " + name);
 //     }
 
+//     confirm()
+    
 // }
+// //console.log(b);
+
+// //if i add numbers and nothing else, I will get an NAN (not a number) error. i.e. running lesson = undefined + 2 vs running lesson(3) = 5
+
+
+// //email validation
+// // function emailValidation() {
+// //     let email = document.getElementById("eField").value;
+
+// //     if (email.includes('@') && email=().includes('.')) {
+// //             //true
+    
+// //     } else {
+// //         //false
+// //     }
+
+// // }
 
 
 
